f4a29ade34648b2731df381b2ad619347eb11929
Aziz Kurniawan <116804043+Aziz097@users.noreply.github.com>
2023-04-02 17:11:20 +0700

    Add files via upload

--- file.cpp
+++ file.cpp
@@ -1,31 +1,62 @@
-// Struktur dasar pemograman cpp
+// Operator CPP
+// Aritmatik
+// +	Addition	Adds together two values	x + y
+// -	Subtraction	Subtracts one value from another	x - y
+// *	Multiplication	Multiplies two values	x * y
+// /	Division	Divides one value by another	x / y
+// %	Modulus	Returns the division remainder	x % y
+// ++	Increment	Increases the value of a variable by 1	++x
+// --	Decrement	Decreases the value of a variable by 1	--x
+
+// Komparasi
+// ==	Equal to	x == y
+// !=	Not equal	x != y
+// >	Greater than	x > y
+// <	Less than	x < y
+// >=	Greater than or equal to	x >= y
+// <=	Less than or equal to	x <= y
+
+// Logika
+// && 	Logical and	Returns true if both statements are true	x < 5 &&  x < 10
+// || 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4
+// !	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
+
 #include <iostream>
 using namespace std;
+
 int main()
 {
-  // Kamus variabel tipe data
-  int myNum = 5;             // Integer (bilangan bulat)
-  float myFloatNum = 5.99;   // Float (bilangan desmial dengan maksimal 7 angka di belakang koma(,))
-  double myDoubleNum = 9.98; // Double (2x float dengan maksimal 15 angka di belakang koma(,))
-  char myLetter = 'D';       // Char (karakter)
-  bool myBoolean = true;     // Boolean (true/false)
-  string myText = "Hello";   // String (kata)
+  int sum1 = 100 + 50;    // 150 (100 + 50)
+  int sum2 = sum1 + 250;  // 400 (150 + 250)
+  int sum3 = sum2 + sum2; // 800 (400 + 400)
 
-  // Algoritma
-  int A, B, C = 7, D;
-  A = 10;
-  cout << "Masukan nilai variabel B = ";
-  cin >> B;
-  cout << A << " " << B << " " << C << endl;
-  D = A;
-  A = B;
-  B = C;
-  C = D;
-  cout << A << " " << B << " " << C << endl;
-  cout << myText << " :" << myLetter;
+  cout << sum1 << endl;
+  cout << sum2 << endl;
+  cout << sum3 << endl;
+
+  int x = 10;
+  int y = 3;
+  int z = x / y;
+  cout << z << endl;
+
+  // menghitung luas lingkaran
+  const float PI = 3.14;
+  float R, luas, keliling;
+
+  cout << "Inputkan N: ";
+  cin >> R;
+
+  cout << "\n";
+  cout << "Menghitung luas lingkaran";
+  luas = PI * R * R;
+  keliling = 2 * PI * R;
+
+  cout << "\n";
+  cout << "Luas : " << luas;
+  cout << "\n";
+  cout << "Keliling : " << keliling;
+
   cout << endl;
-  cout << myFloatNum << " + " << myDoubleNum << " = ";
-  cout << myFloatNum + myDoubleNum;
 
   return 0;
 }
